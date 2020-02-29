713dc66be6118571e8cc7d69ef9080f2674e33aa
Ronda D Cowen <cowenhome5@gmail.com>
2020-02-29 00:17:06 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -6,7 +6,12 @@ class Pokemon
   end
 
   def self.save(pk_name, pk_type, db)
-    
+    sql = <<-SQL
+      INSERT INTO pokemons (pk_name, pk_type)
+      VALUES (?, ?)
+    SQL
+  DB[:conn].execute(sql, self.name, self.grade)
+  @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
 
   end
 
